036d31c25b6c43544e0b20e7feedfc7ad35a2ad4
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-07-17 19:40:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,44 +1,33 @@
 //your JS code here. If required.
 
 function show(username){
- 
-  alert("Logged in as .");
+  alert("Logged in as " + username.value);
 }
-
-
-let event1;
 
 function check(e){
-  console.log("we are inside the check function")
- e.preventDefault();
-  let username=document.getElementById("username");
-let password=document.getElementById("password");
-console.log(`usre name is ${username.value} and pass is ${password.value}`)
- 
-event1=document.getElementById("checkbox").checked;
-  console.log("event1 is ",event1);
+  e.preventDefault();
+  let username = document.getElementById("username");
+  let password = document.getElementById("password");
+  let event1 = document.getElementById("checkbox").checked;
 
-  if(localStorage.getItem("username")==username.value && localStorage.getItem("password")==password.value){
-    let but=document.createElement("button");
-    but.id="existing";
-    but.onclick=()=>{show(username)};
-    but.textContent = "Login as existing user."
-
-     document.getElementById("form").appendChild(but);
-}
-else{
-  if(event1){
- localStorage.setItem("username",username.value);
- localStorage.setItem("password",password.value);
- console.log("inlocal storage data are set ")
+  if(localStorage.getItem("username") == username.value && localStorage.getItem("password") == password.value){
+    let but = document.createElement("button");
+    but.id = "existing";
+    but.onclick = () => {show(username)};
+    but.textContent = "Login as existing user.";
+    document.getElementById("form").appendChild(but);
   }
-  else{
+  else {
+    if(event1) {
+      localStorage.setItem("username", username.value);
+      localStorage.setItem("password", password.value);
+    }
+    else {
       localStorage.removeItem("username");
       localStorage.removeItem("password");
-      console.log("in local storage data removed ")
+    }
+    show(username);
   }
-  alert("Logged in as .")
-}
 }
 
-document.getElementById("submit").addEventListener('click',(e)=>check(e))
+document.getElementById("submit").addEventListener('click', check);
