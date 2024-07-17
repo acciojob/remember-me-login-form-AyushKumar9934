cf4698e2772da2804a5effd9d0e092e01d7e4e4b
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-07-17 19:28:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,44 @@
 //your JS code here. If required.
 
-function show(){
-    alert("Logged in as ."+username.value);
+function show(username){
+ 
+  alert("Logged in as .");
 }
-document.getElementById("submit").addEventListener('click',check)
-let username=document.getElementById("username");
-let password=document.getElementById("password");
+
+
 let event1;
 
-function check(event){
-    event.preventDefault();
- event1=document.getElementById("checkbox").checked;
-    console.log("event1 is ",event1);
-    console.log("we are inside the check function")
-    if(localStorage.getItem("username")==username.value && localStorage.getItem("password")==password.value){
-       document.getElementById("form").innerHTML+=`<button id="existing" onclick="show()">Login as existing user.</button>`
+function check(e){
+  console.log("we are inside the check function")
+ e.preventDefault();
+  let username=document.getElementById("username");
+let password=document.getElementById("password");
+console.log(`usre name is ${username.value} and pass is ${password.value}`)
+ 
+event1=document.getElementById("checkbox").checked;
+  console.log("event1 is ",event1);
+
+  if(localStorage.getItem("username")==username.value && localStorage.getItem("password")==password.value){
+    let but=document.createElement("button");
+    but.id="existing";
+    but.onclick=()=>{show(username)};
+    but.textContent = "Login as existing user."
+
+     document.getElementById("form").appendChild(but);
 }
 else{
-    if(event1){
-   localStorage.setItem("username",username.value);
-   localStorage.setItem("password",password.value);
-   console.log("inlocal storage data are set ")
-    }
-    else{
-        localStorage.removeItem("username");
-        localStorage.removeItem("password");
-        console.log("in local storage data removed ")
-    }
-    alert("Logged in as ."+username.value)
+  if(event1){
+ localStorage.setItem("username",username.value);
+ localStorage.setItem("password",password.value);
+ console.log("inlocal storage data are set ")
+  }
+  else{
+      localStorage.removeItem("username");
+      localStorage.removeItem("password");
+      console.log("in local storage data removed ")
+  }
+  alert("Logged in as .")
 }
 }
+
+document.getElementById("submit").addEventListener('click',(e)=>check(e))
